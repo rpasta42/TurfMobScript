c34921b480873c15f6f69a9cbfe6be4f06ed30de
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-06 12:57:18 -0400

    Trying to write to stdin of adb shell.

--- adblib.py
+++ adblib.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-#from pycloak import shellutils as sh
-import sh
+from subprocess import Popen, PIPE, STDOUT
 
-#adb_path = '/usr/bin/adb'
+#from pycloak import shellutils as sh
+#import sh
+adb_path = '/usr/bin/adb'
 
 def adb_cmd(cmd):
+   cmd += '; exit'
+   print('running command: ' + cmd)
+   #p = Popen(['adb', 'shell'], stdout=PIPE, stdin = PIPE, stderr=STDOUT)
+   p = Popen(['adb', 'shell'], stdout = PIPE, stdin = PIPE, stderr = STDOUT)
+   #std_out = p.communicate(input=bytes(cmd, 'utf-8'))
+   #print('adb out: ' + std_out)
    #cmd = '%s %s' % (adb_path, cmd)
    #sh.exec_prog(cmd)
-   print(cmd)
-   sh.adb(cmd)
+   #sh.adb(cmd)
 
 def click(x, y):
    adb_cmd('input tap %s %s' % (x, y))
