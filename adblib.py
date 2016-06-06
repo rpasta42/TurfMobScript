c359775c197d78f2537ffb8d2dfc47f626caf8f7
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-06 13:29:46 -0400

    Got basic touch to work.

--- adblib.py
+++ adblib.py
@@ -6,13 +6,19 @@ from subprocess import Popen, PIPE, STDOUT
 #import sh
 adb_path = '/usr/bin/adb'
 
+def adb_cmd1(cmd):
+   #print(sh.adb("shell", sh.echo(cmd + ';exit')))
+   #print(sh.adb("shell", sh.echo("ls")))
+   #print(sh.adb('shell ' + cmd))
+   pass
+
 def adb_cmd(cmd):
-   cmd += '; exit'
+   cmd += '; exit\n'
    print('running command: ' + cmd)
    #p = Popen(['adb', 'shell'], stdout=PIPE, stdin = PIPE, stderr=STDOUT)
-   p = Popen(['adb', 'shell'], stdout = PIPE, stdin = PIPE, stderr = STDOUT)
-   #std_out = p.communicate(input=bytes(cmd, 'utf-8'))
-   #print('adb out: ' + std_out)
+   p = Popen([adb_path, 'shell'], stdout = PIPE, stdin = PIPE, stderr = STDOUT)
+   std_out = p.communicate(input=bytes(cmd))[0]
+   print('adb out: ' + std_out)
    #cmd = '%s %s' % (adb_path, cmd)
    #sh.exec_prog(cmd)
    #sh.adb(cmd)
@@ -20,7 +26,7 @@ def adb_cmd(cmd):
 def click(x, y):
    adb_cmd('input tap %s %s' % (x, y))
 
-click(100, 100)
 
 if __name__ == "__main__":
-   print('hello')
+   click(100, 100)
+   click(0, 0)
