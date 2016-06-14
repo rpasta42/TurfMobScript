f27fc8d98cd37c7c178e6837c1208b78e060f5ff
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-14 18:52:15 -0400

    Fixed to print to sreen instead of csv file.

--- hardhax/cookie.sh
+++ hardhax/cookie.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-adb shell su -c "cp /data/data/com.meanfreepathllc.turfwars/app_webview/Cookies /sdcard"
-adb shell su -c "chmod a+r /sdcard/Cookies"
+adb shell su -c "cp /data/data/com.meanfreepathllc.turfwars/app_webview/Cookies /sdcard" >/dev/null
+adb shell su -c "chmod a+r /sdcard/Cookies" >/dev/null
 adb pull /sdcard/Cookies
 mv Cookies /tmp
 
